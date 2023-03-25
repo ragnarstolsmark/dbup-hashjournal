c73bb26c776cb3163a079e56cf4a6215c3e158f9
Ragnar Stølsmark <rstolsmark@live.no>
2023-03-25 19:10:45 +0100

    Added hash journal

---
+++ src/DbUp.HashJournal.Core/HashJournalDefaults.cs
@@ -0,0 +1,9 @@
+using System.Security.Cryptography;
+
+namespace DbUp.HashJournal.Core
+{
+    public class HashJournalDefaults
+    {
+        public const string NameHashSeparator = "|";
+    }
+}
---
+++ src/DbUp.HashJournal.Core/MD5Hasher.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace DbUp.HashJournal.Core
+{
+    public static class MD5Hasher
+    {
+        public static string GetHash(string input)
+        {
+            using (var hasher = MD5.Create())
+            {
+                var inputBytes = Encoding.UTF8.GetBytes(input);
+                var hash = hasher.ComputeHash(inputBytes);
+                return BitConverter.ToString(hash).Replace("-", String.Empty).ToLowerInvariant();
+            }
+        }
+    }
+}
